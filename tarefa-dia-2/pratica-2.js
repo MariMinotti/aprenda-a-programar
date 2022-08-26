85563b26d3d6329ca4fccfbcc6da0cc8245f5d5e
MariMinotti <minotti.m93@gmail.com>
2022-08-26 17:09:26 -0300

    Adicionei um texto de email

---
+++ tarefa-dia-2/pratica-2.js
@@ -0,0 +1,7 @@
+let nome = "Mariana"
+let email = "minotti.m923@gmail.com"
+let profissao =  "aai"
+let dreamplace = "paris"
+let idade =  28 
+
+let mensagem = "Olá" + nome + "! Somos aqui da agência de turismo  Viagem dos Sonhos.  Estamos te escrevendo este email , pois acabamos de confirmar as compras das passagens aéreas para " + dreamplace + ". Confirma pra gente alguns dados? A sua idade é" + idade + ",você é" + profissao + "e o seu email é " + email + "? Ficamos no aguardo . Muito obrigado e boa viagem" 
