30183f84e728fd0c35d6ec1ab429d91824232fec
MariMinotti <minotti.m93@gmail.com>
2022-08-26 17:14:59 -0300

    Adicionei o comando console.log()

--- tarefa-dia-2/pratica-2.js
+++ tarefa-dia-2/pratica-2.js
@@ -5,3 +5,5 @@ let dreamplace = "paris"
 let idade =  28 
 
 let mensagem = "Olá" + nome + "! Somos aqui da agência de turismo  Viagem dos Sonhos.  Estamos te escrevendo este email , pois acabamos de confirmar as compras das passagens aéreas para " + dreamplace + ". Confirma pra gente alguns dados? A sua idade é" + idade + ",você é" + profissao + "e o seu email é " + email + "? Ficamos no aguardo . Muito obrigado e boa viagem" 
+
+console.log(mensagem)
