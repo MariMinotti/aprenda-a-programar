6fc3aa95b7ad0a1add2b513a3a80934154cb6dd3
MariMinotti <minotti.m93@gmail.com>
2022-08-27 19:14:40 -0300

    Fiz a tabuada de 30!

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -1,8 +1,8 @@
 let contador = 1
 let n = 20 
-while (contador <= n) {
-    console.log(contador)
-    contador = contador + 1
+while (contador <= n) {      // while (i <= n) {
+    console.log(contador)    // console.log(i)  
+    contador = contador + 1   // i++ 
 }
 
 // n = 20  
