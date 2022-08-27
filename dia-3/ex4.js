5940fd2e11ffa5a7c559c04773f86e317efd75eb
MariMinotti <minotti.m93@gmail.com>
2022-08-27 15:30:01 -0300

    Usei if e else para saber se era string ou number

--- dia-3/ex4.js
+++ dia-3/ex4.js
@@ -1,4 +1,4 @@
-let valor = 2
+let valor = 1 != 1 
 let tipoDoValor = typeof(valor)
 
 console.log(`${valor} é do tipo ${tipoDoValor}`)
---
+++ dia-3/ex5.js
@@ -0,0 +1,8 @@
+let valor = "12" 
+let tipoDoValor =typeof(valor)
+
+if (typeof(valor) == "number") {
+    console.log(`${valor} é do tipo ${tipoDoValor}`)
+} else {
+    console.log(`${valor} não é do tipo number`)
+}
