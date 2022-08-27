256ac344dc2526495dd9d349b040443a15f53c9c
MariMinotti <minotti.m93@gmail.com>
2022-08-27 11:38:17 -0300

    Adicionei string templates

--- dia-3/ex1.js
+++ dia-3/ex1.js
@@ -4,5 +4,8 @@ let idade = 17
 if (idade >= 18) {
    console.log("Pode entrar " + nome)
 } else {
-    console.log("Acesso negado para " + nome)
+
 }
+
+
+
---
+++ dia-3/ex2.js
@@ -0,0 +1,9 @@
+
+let nome = "Carlos"
+let idade =  12 
+
+if (idade>=18){
+    console.log(`${nome} pode entrar`)
+} else {
+    console.log(`${nome} desculpa mas você não pode entrar, pois ${idade} anos é menor que 18 anos`)
+}
