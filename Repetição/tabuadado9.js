d0b413e20c1f317c5d700b75edd1c1fe24804b10
MariMinotti <minotti.m93@gmail.com>
2022-08-31 18:33:33 -0300

    Fiz a tabuada do 9

---
+++ Repetição/tabuadado9.js
@@ -0,0 +1,10 @@
+let multiplicador =  9  
+i = 1 
+n = 20  
+
+while (i <= n) {
+    let resultado =  multiplicador * i
+    let textoExibicao =  `${multiplicador} X ${i} =  ${resultado}`
+    console.log(textoExibicao)
+    i++
+}
